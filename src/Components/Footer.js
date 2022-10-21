cfac78545d39eb80c6e7b05f6146985a2e64e69a
sevilcankiz <kumral-sevil21@hotmail.de>
2022-10-21 17:57:43 +0200

    header weiter bearbeitet

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../Styles/footer.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <ul>
+        
+        <li>Suchergebnisse</li>
+        
+        <li>Bewerbung</li>
+        <li>Dokumente</li>
+    </ul>
+    </div>
+  )
+}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import "../Styles/header.css";
 
 export default function Header() {
@@ -13,19 +13,17 @@ export default function Header() {
             setHamburger(!isHamburger)}}>
             nav
         </button>
-        <div class={ isHamburger ? "nav-menu expanded" : "nav-menu" }>
-            <input className="search" type="text" placeholder="Jobtitle"></input>
-            <button>search</button>
-            <ul>
-                
-                    <li>LogIn</li>
-                
-                
-                    <li>LogOut</li>
-                
 
-                    <li>Info</li>
-                
+        <div class={ isHamburger ? "nav-menu expanded" : "nav-menu" }>
+            <div className="searchbar">
+                <input className="search" type="text" placeholder="Jobtitle"></input>
+                <input type="number" placeholder="Plz"></input>
+                <button>search</button>
+            </div>
+            <ul>
+                <li>LogIn</li>
+                <li>LogOut</li>
+                <li>Info</li>
             </ul>
         </div>
     </nav>
