3339010dd3dfc8feb904788cc5e673a555b6f1aa
sevilcankiz <kumral-sevil21@hotmail.de>
2022-10-24 17:42:30 +0200

    am responsiv gearbeitet

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import "../Styles/footer.css";
 
+
 export default function Footer() {
   return (
     <div>
-      <ul>
-        
+      <ul className='footer'>
         <li>Suchergebnisse</li>
-        
         <li>Bewerbung</li>
         <li>Dokumente</li>
     </ul>
