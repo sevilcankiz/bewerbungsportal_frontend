7993361a8806b3f921b4c3558500658cf6f3d987
sevilcankiz <kumral-sevil21@hotmail.de>
2022-11-03 14:17:09 +0100

    infe zu bewerbung geändert

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -19,13 +19,13 @@ export default function Navigation({ isAuthenticated, logOut, user }) {
                      <>
                      <li className='nutzer'><NavLink to="/user">Nutzerdaten</NavLink></li>
                     <li onClick={logOut}>Logout</li>
-                    <li className='info'><NavLink to="/user/main">Info</NavLink></li>
+                    <li className='info'><NavLink to="/user/main">Bewerbung</NavLink></li>
                      </>
                 ) : (
                     <>
                     <li className='nutzer'><NavLink to="/login">Login</NavLink></li>
                     <li><NavLink to="/register">Registrierung</NavLink></li>
-                    <li className='info'><NavLink to="/">Info</NavLink></li>
+                    <li className='info'><NavLink to="/">Bewerbung</NavLink></li>
                     </>
                 )}
                 </ul>
